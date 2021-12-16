afe8f6781b083e90e814dfbb5ba0de269ccdd705
jessica de Jesus Santos <jessicasantos1856@gmail.com>
2021-12-16 17:28:27 -0300

    cria a aleatoridade da frutinha e tamanho da cobrinha

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ snake[0] = {
 
 let direction = 'right';
 let food = {
-  x:Math.floor(Math.random() * 15 + 1) * box,
-  y:Math.floor(Math.random() * 15 + 1) * box
-}
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box,
+};
 
 function criarBG() {
   context.fillStyle = 'lightgreen';
@@ -40,10 +40,10 @@ function update(event) {
 }
 
 function iniciarJogo() {
-  if(snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
-  if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
-  if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
-  if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+  if (snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
+  if (snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+  if (snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
+  if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
 
   criarBG();
   criarCobrinha();
@@ -57,7 +57,13 @@ function iniciarJogo() {
   if (direction == 'up') snakeY -= box;
   if (direction == 'down') snakeY += box;
 
-  snake.pop();
+  if (snakeX != food.x || snakeY != food.y) {
+    snake.pop();
+  } 
+  else {
+    food.x = Math.floor(Math.random() * 15 + 1) * box,
+    food.y = Math.floor(Math.random() * 15 + 1) * box;
+  }
 
   let newHead = {
     x: snakeX,
